c8b1fab7abe5a0f1a6432fb37b26195f4c91d599
ShashwataDas97 <142168847+ShashwataDas97@users.noreply.github.com>
2023-08-13 21:59:05 +0530

    Add files via upload

---
+++ Snake2DAccio/src/SnakeGame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class SnakeGame extends JFrame {
+    Board board;
+    SnakeGame() {
+        board = new Board();
+        setTitle("Snake Game");
+        add(board);
+        pack();
+        setResizable(false);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        SnakeGame snakeGame = new SnakeGame();
+    }
+}
